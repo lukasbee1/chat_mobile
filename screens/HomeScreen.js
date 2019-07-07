162a475a5bdf178382c08d0c3226fd8501ed8834
Lukas <lukashevich2014@gmail.com>
2019-07-08 00:13:51 +0300

    signIn pages

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
-export default function HomeScreen() {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
+export class HomeScreen extends React.PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>HomeScreen</Text>
+        <Button
+          title="Sign Out"
+          onPress={() => this.props.navigation.navigate('SignIn')}
+        />
+      </View>
+    );
+  }
 }
+export default HomeScreen;
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,23 +1,81 @@
-import React, { PureComponent } from 'react';
-import { View } from 'react-native';
-import { Button, Input } from 'react-native-elements';
+import React, { Component } from 'react';
 
-export class SignIn extends PureComponent {
+import styles from '../constants/Styles';
+import {
+  Keyboard,
+  Text,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { Button } from 'react-native-elements';
+
+const appId = '1047121222092614';
+
+export default class SignIn extends Component {
   render() {
     return (
-      <View style={{ paddingVertical: 100 }}>
-        <Input label="Email" placeholder="Email address..." />
-        <Input label="Password" secureTextEntry placeholder="Password..." />
-
-        <Button
-          buttonStyle={{ marginTop: 20 }}
-          backgroundColor="#03A9F4"
-          title="SIGN IN"
-          onPress={() => this.props.navigation.navigate('SignedIn')}
-        />
-      </View>
+      <KeyboardAvoidingView
+        style={styles.containerView}
+        behavior="padding"
+        enabled
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.loginFormView}>
+            <View style={styles.loginScreenContainer}>
+              <Text style={styles.logoText}>Luxas Chat</Text>
+              <TextInput
+                placeholder="Username"
+                placeholderColor="#c4c3cb"
+                style={styles.loginFormTextInput}
+              />
+              <TextInput
+                placeholder="Password"
+                placeholderColor="#c4c3cb"
+                style={styles.loginFormTextInput}
+                secureTextEntry={true}
+              />
+              <Button
+                buttonStyle={styles.loginButton}
+                onPress={() => this.onLoginPress()}
+                title="Login"
+              />
+              <Button
+                buttonStyle={styles.loginButton}
+                onPress={() => this.props.navigation.push('SignUp')}
+                title="Sign Up"
+              />
+              <Button
+                buttonStyle={styles.fbLoginButton}
+                onPress={() => this.onFbLoginPress()}
+                title="Login with Facebook"
+                color="#3897f1"
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
-}
 
-export default SignIn;
+  onLoginPress() {
+    this.props.navigation.navigate('SignedIn');
+  }
+
+  async onFbLoginPress() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      appId,
+      {
+        permissions: ['public_profile', 'email'],
+      }
+    );
+    if (type === 'success') {
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+    }
+  }
+}
